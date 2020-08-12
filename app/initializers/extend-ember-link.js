6b821c4d4e30c62e9c92d9d596e8b83b2d080ea6
DaiYuzeng <yuzeng@rancher.com>
2020-08-12 13:54:05 +0800

    fix(extend-ember-link): version rollback

--- app/initializers/extend-ember-link.js
+++ app/initializers/extend-ember-link.js
@@ -1,7 +1,5 @@
 import LinkComponent from '@ember/routing/link-component';
-import { get } from '@ember/object';
-import { on } from '@ember/object/evented';
-import $ from 'jquery';
+import { get } from '@ember/object'
 
 export function initialize(/* application */) {
   LinkComponent.reopen({
@@ -11,20 +9,20 @@ export function initialize(/* application */) {
     // class to the parent element of that tag name (like <li>{{link-to}}</li>)
     activeParent: null,
 
-    addActiveObserver: on('didInsertElement', function() {
+    addActiveObserver: function() {
       if ( this.get('activeParent') ) {
         this.addObserver('active', this, 'activeChanged');
         this.addObserver('application.currentRouteName', this, 'activeChanged');
         this.activeChanged();
       }
-    }),
+    }.on('didInsertElement'),
 
     activeChanged() {
       if ( this.isDestroyed || this.isDestroying ) {
         return;
       }
 
-      const parent = $().closest(get(this, 'activeParent'));
+      const parent = this.$().closest(get(this, 'activeParent'));
 
       if ( !parent || !parent.length ) {
         return;
