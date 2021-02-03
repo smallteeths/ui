65879430b3a0829578eccc42f288cffc4bab133e
yuzeng <cridequeger@yahoo.com>
2021-02-03 10:33:20 +0800

    Redirect to workloads when switch to the cluster that disabled F5CIS

--- app/authenticated/project/f5/index/route.js
+++ app/authenticated/project/f5/index/route.js
@@ -1,17 +1,27 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 import C from 'ui/utils/constants';
 import { on } from '@ember/object/evented';
-import { set } from '@ember/object';
+import { get, set } from '@ember/object';
+import { alias } from '@ember/object/computed';
 
 const DEFAULT_ROUTE = 'authenticated.project.f5.controllers';
 const VALID_ROUTES = [DEFAULT_ROUTE, 'authenticated.project.f5.tls'];
 
 export default Route.extend({
+  scope:          service(),
+  currentCluster: alias('scope.currentCluster'),
+
   redirect() {
+    const enabledF5 = get(this, 'currentCluster.enableF5CIS');
     let route = this.get(`session.${ C.SESSION.F5_ROUTE }`);
 
     if ( !VALID_ROUTES.includes(route) ) {
       route = DEFAULT_ROUTE;
+    }
+
+    if (!enabledF5) {
+      route = 'authenticated.project.index'
     }
 
     this.replaceWith(route);
