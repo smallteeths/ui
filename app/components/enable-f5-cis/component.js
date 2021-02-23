a6b3445266e6c7649162afb44b6c8494a28a4115
yuzeng <cridequeger@yahoo.com>
2021-02-23 09:13:44 +0800

    add yaml editor and custom answers

--- app/components/enable-f5-cis/component.js
+++ app/components/enable-f5-cis/component.js
@@ -6,15 +6,28 @@ import {
   set, get, computed, observer, setProperties
 } from '@ember/object';
 import { alias } from '@ember/object/computed';
+import { convertDotAnswersToBoolYaml } from 'shared/utils/convert-yaml';
+import jsyaml from 'js-yaml';
+import flatMap, { isEmptyObject, isObject } from 'shared/utils/flat-map';
 import layout from './template';
+import InputAnswers from 'shared/mixins/input-answers';
 
+const EXPOSED_OPTIONS = [
+  'bigip.password',
+  'bigip.url',
+  'bigip.username',
+  'network.flannelName',
+  'network.poolMemberType',
+  'network.type',
+  'partition'
+];
 
 const NETWORK_TYPE_CHOISES = [
   {
     label: 'Flannel + Rancher Macvlan',
     value: 'flannel'
   }
-]
+];
 
 const POOL_MEMBER_TYPE_CHOISES = [
   {
@@ -25,15 +38,16 @@ const POOL_MEMBER_TYPE_CHOISES = [
     label: 'NodePort',
     value: 'nodeport'
   },
-]
+];
 
-export default Component.extend({
+export default Component.extend(InputAnswers, {
   scope: service(),
   intl:  service(),
 
   layout,
 
   answers:        null,
+  customAnswers:  null,
   justDeployed:   false,
   confirmDisable: false,
   errors:         [],
@@ -43,9 +57,14 @@ export default Component.extend({
 
   poolMemberType: 'cluster',
   networkType:    'flannel',
+  url:            '',
+  partition:      '',
+  username:       '',
+  password:       '',
 
-  cluster: alias('scope.currentCluster'),
-  version: alias('versionConfig.defaultVersion'),
+  cluster:    alias('scope.currentCluster'),
+  version:    alias('versionConfig.defaultVersion'),
+  valuesYaml: alias('pastedAnswers'),
 
   actions: {
     promptDisable() {
@@ -70,7 +89,15 @@ export default Component.extend({
       let action = get(this, 'enabled') ?  'editF5CIS' : 'enableF5CIS';
       const params = {};
 
-      set(params, 'answers', answers);
+      const { valuesYaml, pasteOrUpload } = this
+
+      if (pasteOrUpload) {
+        set(params, 'valuesYaml', valuesYaml)
+      } else {
+        set(params, 'answers', answers)
+      }
+
+      // set(params, 'answers', answers);
       set(params, 'version', get(this, 'version'));
 
       cluster.doAction(action, params).then(() => {
@@ -95,11 +122,27 @@ export default Component.extend({
         }
 
         setProperties(this, {
-          answers: null,
-          version: get(this, 'versionConfig.defaultVersion'),
+          answers:       null,
+          customAnswers: null,
+          version:       get(this, 'versionConfig.defaultVersion'),
         })
       })
     },
+
+    showPaste() {
+      const answers = this.updateAnswers()
+      const yaml = convertDotAnswersToBoolYaml(answers);
+
+      setProperties(this, {
+        pastedAnswers: yaml,
+        pasteOrUpload: true,
+      })
+    },
+
+    cancel() {
+      this.parseYamlAnswers();
+      this.filterMissingAnswer();
+    }
   },
 
   enabled: computed('cluster.enableF5CIS', function() {
@@ -164,19 +207,35 @@ export default Component.extend({
           return;
         }
 
+        const { valuesYaml } = res
         const body = get(res, 'answers');
         const version = get(res, 'version');
         const answers = {};
+        const customAnswers = {};
 
         Object.keys(body || {}).forEach((key) => {
-          answers[key] = body[key];
+          if ( EXPOSED_OPTIONS.indexOf(key) > -1 ) {
+            answers[key] = body[key];
+          } else {
+            customAnswers[key] = body[key];
+          }
         });
 
-        setProperties(this, {
-          answers,
-          version
-        });
-        this.updateConfig(answers);
+        if (valuesYaml) {
+          setProperties(this, {
+            version,
+            pasteOrUpload: true,
+            pastedAnswers: valuesYaml,
+          })
+          this.parseYamlAnswers()
+        } else {
+          setProperties(this, {
+            version,
+            answers,
+            customAnswers,
+          })
+          this.updateConfig(answers);
+        }
 
         set(this, 'loading', false);
       }).catch(() => {
@@ -215,7 +274,84 @@ export default Component.extend({
       }
     }
 
+    const customAnswers = get(this, 'customAnswers') || {};
+
+    Object.keys(customAnswers).forEach((key) => {
+      answers[key] = customAnswers[key]
+    });
+
     return answers
+  },
+
+  parseYamlAnswers() {
+    let { valuesYaml } = this;
+    let parsedYaml     = null;
+
+    try {
+      parsedYaml = jsyaml.safeLoad(valuesYaml);
+    } catch ( err ) {
+      set(this, 'yamlErrors', [`YAML Parse Error: ${ err.snippet } - ${ err.message }`]);
+    }
+
+    if (parsedYaml) {
+      let answers = flatMap(parsedYaml);
+
+      this.updateConfig(answers)
+
+      const customAnswerKeys = Object.keys(answers).filter((a = '') => {
+        const filter = EXPOSED_OPTIONS.filter((q) => a.startsWith(q))
+
+        if (get(filter, 'length') > 0) {
+          return false
+        } else {
+          return true
+        }
+      })
+
+      const customAnswers = {}
+
+      customAnswerKeys.map((key) => {
+        customAnswers[key] = answers[key]
+      })
+
+      set(this, 'customAnswers', customAnswers)
+    }
+  },
+
+  filterMissingAnswer() {
+    const customAnswers = get(this, 'customAnswers') || {}
+
+    const missingKeys = Object.keys(customAnswers).map((key) => {
+      const value = customAnswers[key]
+
+      if (isObject(value) && isEmptyObject(value)) {
+        return key
+      }
+    }).filter((m) => m)
+
+    if (missingKeys.length > 0) {
+      let newCustomAnswers = {}
+
+      Object.keys(customAnswers).map((key) => {
+        if (!missingKeys.includes(key)) {
+          newCustomAnswers[key] = customAnswers[key]
+        }
+      })
+
+      set(this, 'customAnswers', newCustomAnswers)
+
+      this.modalService.toggleModal('modal-confirm-yaml-switch', {
+        finish:                  this.finishBackToForm.bind(this),
+        propertiesGoingToBeLost: missingKeys.map((key) => {
+          return {
+            lostKey:   key,
+            lostValue: '{}'
+          }
+        }),
+      })
+    } else {
+      this.finishBackToForm()
+    }
   },
 
 });
