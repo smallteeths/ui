933cc81039fba79341664aae2a4462ced9cfd7a5
smallteeths <503630985@qq.com>
2021-03-09 08:49:36 +0800

    feat(password) Add first-time login password strength prompt

--- app/components/input-edit-password/component.js
+++ app/components/input-edit-password/component.js
@@ -119,6 +119,62 @@ export default Component.extend({
     }
   }),
 
+  passwordStrengthValue: computed('password', function() {
+    let modes = 0;
+    let password = get(this, 'password') ? get(this, 'password') : ''
+
+    if (password.length < 1) {
+      return modes;
+    }
+    if (password.length < 6 && password.length > 1) {
+      return 'weak';
+    }
+    (/\d/.test(password)) && modes++;
+    (/[a-z]/.test(password)) && modes++;
+    (/[A-Z]/.test(password)) && modes++;
+    (/\W/.test(password)) && modes++;
+
+    switch (modes) {
+    case 1:
+      return 'weak';
+    case 2:
+      return 'good';
+    case 3:
+    case 4:
+      return 'best';
+    }
+
+    return modes;
+  }),
+
+  passwordStrengthClass: computed('passwordStrengthValue', function() {
+    switch (get(this, 'passwordStrengthValue')) {
+    case 'weak':
+      return 'password-meter-weak';
+    case 'good':
+      return 'password-meter-good';
+    case 'best':
+      return 'password-meter-best';
+    }
+
+    return 'password-meter-null'
+  }),
+
+  passwordStrengthText: computed('passwordStrengthValue', function() {
+    const intl = get(this, 'intl');
+
+    switch (get(this, 'passwordStrengthValue')) {
+    case 'weak':
+      return intl.t('modalEditPassword.strength.weak');
+    case 'good':
+      return intl.t('modalEditPassword.strength.good');
+    case 'best':
+      return intl.t('modalEditPassword.strength.best');
+    }
+
+    return ''
+  }),
+
   saveDisabled: computed('generate', 'passwordsMatch', 'forceSaveDisabled', 'showCurrent', 'currentPassword', function() {
     if ( get(this, 'forceSaveDisabled') ) {
       return true;
