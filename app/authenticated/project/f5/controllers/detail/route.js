276817666fdeb59400c1346801c5ff11fbf81385
yuzeng <cridequeger@yahoo.com>
2021-03-12 15:27:38 +0800

    fix bugs caused by switching from workload to service

--- app/authenticated/project/f5/controllers/detail/route.js
+++ app/authenticated/project/f5/controllers/detail/route.js
@@ -5,13 +5,15 @@ import { hash } from 'rsvp';
 export default Route.extend({
   model(params) {
     const store = get(this, 'store');
-    let f5 = store.find('virtualServer', params.controller_id);
+    let f5 = null;
     let isVirtualServer = true;
     const tlsProfiles = store.findAll('tlsprofile');
 
     if (params.type === 'transportServer') {
       f5 = store.find('transportserver', params.controller_id);
       isVirtualServer = false;
+    } else {
+      f5 = store.find('virtualServer', params.controller_id)
     }
 
 
