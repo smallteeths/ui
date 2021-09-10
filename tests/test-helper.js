db49c4f4222232eefe5dda40d6109acd261b1256
LiuYan <361112237@qq.com>
2021-09-10 10:53:16 +0800

    chore: fix merge rancher ui v2.6.0 tag issue
    chore: fix some page header style error

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -4,6 +4,8 @@ import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
+import preloadAssets from 'ember-asset-loader/test-support/preload-assets';
+import manifest from 'ui/config/asset-manifest';
 
 setApplication(Application.create(config.APP));
 
