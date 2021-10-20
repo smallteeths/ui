968547f322ad55c909160296440f6dc8df49e0fc
LiuYan <361112237@qq.com>
2021-10-20 16:34:09 +0800

    fix(config-maps/edit): fix page jump error after click save button

--- app/authenticated/project/config-maps/detail/edit/controller.js
+++ app/authenticated/project/config-maps/detail/edit/controller.js
@@ -3,7 +3,7 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   actions: {
     back() {
-      this.send('goToPrevious', 'authenticated.project.config-maps.index');
+      this.transitionToRoute('authenticated.project.config-maps');
     },
   },
 });
