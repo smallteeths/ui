5296248d02202cb1b1c25d6cbdfca4c83cbd0809
LiuYan <361112237@qq.com>
2021-11-02 10:50:15 +0800

    fix(dns/new): set clusterIp to None if clone dns for new

--- app/authenticated/project/dns/new/route.js
+++ app/authenticated/project/dns/new/route.js
@@ -35,6 +35,9 @@ export default Route.extend({
 
       if ( hash.existing ) {
         record = hash.existing.cloneForNew();
+        if (record.clusterIp && record.clusterIp !== 'None') {
+          record.clusterIp = '';
+        }
         delete hash.existing;
       } else {
         record = store.createRecord({
