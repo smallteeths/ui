e5e86d3a36980086713caa5f56158b865d1fbb4e
LiuYan <361112237@qq.com>
2021-11-16 18:08:01 +0800

    fix(secrets-edit): fix goback error

--- app/authenticated/project/secrets/detail/edit/controller.js
+++ app/authenticated/project/secrets/detail/edit/controller.js
@@ -3,7 +3,7 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   actions: {
     back() {
-      this.send('goToPrevious', 'authenticated.project.secrets');
+      this.transitionToRoute('authenticated.project.secrets');
     },
   },
 });
