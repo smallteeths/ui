64f28d8c81dab7283f680264548de54c376e3c62
LiuYan <361112237@qq.com>
2021-12-22 14:02:36 +0800

    feat(container-default-limit): support min/max cpu/memory limit
    issue: https://github.com/cnrancher/pandaria/issues/1251

--- app/components/container-default-limit/component.js
+++ app/components/container-default-limit/component.js
@@ -4,6 +4,21 @@ import { convertToMillis } from 'shared/utils/util';
 import { parseSi } from 'shared/utils/parse-unit';
 import layout from './template';
 
+const RESOURCE_LIMIT_KEYS = {
+  cpu: [
+    'limitsCpu',
+    'requestsCpu',
+    'minCpu',
+    'maxCpu',
+  ],
+  memory: [
+    'limitsMemory',
+    'requestsMemory',
+    'minMemory',
+    'maxMemory',
+  ]
+};
+
 export default Component.extend({
   layout,
 
@@ -12,42 +27,33 @@ export default Component.extend({
   init() {
     this._super(...arguments);
 
-    setProperties(this, {
-      requestsCpu:    convertToMillis(get(this, 'limit.requestsCpu')),
-      limitsCpu:      convertToMillis(get(this, 'limit.limitsCpu')),
+    setProperties(this, RESOURCE_LIMIT_KEYS.cpu.reduce((t, k) => {
+      t[k] = convertToMillis(get(this, `limit.${ k }`));
+
+      return t;
+    }, {}));
+
+    RESOURCE_LIMIT_KEYS.memory.forEach((k) => {
+      if (get(this, `limit.${ k }`)) {
+        set(this, k, parseSi(get(this, `limit.${ k }`), 1024) / 1048576);
+      }
     });
-
-    if ( get(this, 'limit.requestsMemory') ) {
-      set(this, 'requestsMemory', parseSi(get(this, 'limit.requestsMemory'), 1024) / 1048576);
-    }
-
-    if ( get(this, 'limit.limitsMemory') ) {
-      set(this, 'limitsMemory', parseSi(get(this, 'limit.limitsMemory'), 1024) / 1048576);
-    }
   },
 
-  limitChanged: observer('requestsCpu', 'limitsCpu', 'requestsMemory', 'limitsMemory', function() {
-    const requestsCpu    = get(this, 'requestsCpu');
-    const limitsCpu      = get(this, 'limitsCpu');
-    const requestsMemory = get(this, 'requestsMemory');
-    const limitsMemory   = get(this, 'limitsMemory');
-    const out            = {};
+  limitChanged: observer(...RESOURCE_LIMIT_KEYS.cpu, ...RESOURCE_LIMIT_KEYS.memory, function() {
+    const out = {};
 
-    if ( requestsCpu ) {
-      set(out, 'requestsCpu', `${ requestsCpu }m`)
-    }
+    RESOURCE_LIMIT_KEYS.cpu.forEach((k) => {
+      if (get(this, k)) {
+        set(out, k, `${ get(this, k) }m`);
+      }
+    });
 
-    if ( limitsCpu ) {
-      set(out, 'limitsCpu', `${ limitsCpu }m`)
-    }
-
-    if ( requestsMemory ) {
-      set(out, 'requestsMemory', `${ requestsMemory }Mi`)
-    }
-
-    if ( limitsMemory ) {
-      set(out, 'limitsMemory', `${ limitsMemory }Mi`)
-    }
+    RESOURCE_LIMIT_KEYS.memory.forEach((k) => {
+      if (get(this, k)) {
+        set(out, k, `${ get(this, k) }Mi`);
+      }
+    });
 
     if (this.changed) {
       this.changed(out);
